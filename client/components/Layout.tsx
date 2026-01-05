be807d05513621cc4c3eaa36d2d2ce6f33cff833
mlnj27 <yjm9575m89@gmail.com>
2026-01-05 14:36:58 +0900

    일단 원래대로

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -165,10 +165,7 @@ export default function Layout({
             </h1>
 
             {isHomepage && (
-              <div
-                className="text-white/90 text-xl md:text-2xl lg:text-3xl font-medium mb-6 md:mb-8
-                     text-center lg:text-left pl-0 lg:pl-8"
-              >
+              <div className="text-white/90 text-xl md:text-2xl lg:text-3xl font-medium mb-6 md:mb-8 text-center lg:text-left pl-8">
                 <span className="block">"맞춤 상담, 사후관리까지 책임지는</span>
                 <span className="block">&nbsp;믿을 수 있는 세무 동반자"</span>
               </div>
